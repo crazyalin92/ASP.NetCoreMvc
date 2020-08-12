e4e8d2b147490a4ed16d1e2f136decba64a7d324
Alina Garaeva <crazyalin91@mail.ru>
2020-08-12 13:01:16 +0300

    Initial commit

---
+++ ASP.NETCoreMvc/Models/MobileContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NETCoreMvc.Models
+{
+    public class MobileContext : DbContext
+    {
+        public DbSet<Phone> Phones { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        public MobileContext(DbContextOptions<MobileContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
